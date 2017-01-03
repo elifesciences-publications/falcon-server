98a9920852a5414c5eb40898d02df7f0b4f5157b
Fabian Kloosterman <fabian.kloosterman@nerf.be>
2017-01-03 13:17:08 +0100

    Added copyright notice to source files. Updated dependencies in readme.txt. Moved license file to root folder.

--- tests/processors/multichanneldatafilestreamer/test_multichanneldata_filestreamer.py
+++ tests/processors/multichanneldatafilestreamer/test_multichanneldata_filestreamer.py
@@ -5,6 +5,26 @@ Created on Thu Sep 10 18:18:43 2015
 @author: davide
 """
 
+# ---------------------------------------------------------------------
+# This file is part of falcon-server.
+#
+# Copyright (C) 2015, 2016, 2017 Neuro-Electronics Research Flanders
+#
+# Falcon-server is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Falcon-server is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with falcon-server. If not, see <http://www.gnu.org/licenses/>.
+# ---------------------------------------------------------------------
+
+
 import numpy as np
 from falcon.io.deserialization import load_file
 import config_file as config
